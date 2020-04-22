700ce1da0da66295926a1bdfc7e2c8c97726dc42
Kunal Singh <kunal1998doon@gmail.com>
2020-04-22 12:16:25 +0530

    Added click to move for player

---
+++ Assets/Scripts/RayCastLogic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class RayCastLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    NavMeshAgent agent;
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire2"))
+        {
+            RayCastCameraToMouse();
+        }
+    }
+    void RayCastCameraToMouse()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray,out hit,100.0f))
+        {
+            Debug.Log("Ray hit");
+            agent.SetDestination(hit.point);
+        }
+    }
+}
